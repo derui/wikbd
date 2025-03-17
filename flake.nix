71d479d8caef456512dd3963901a5a36cadf5e7f
derui <derutakayu@gmail.com>
2025-03-17 19:57:11 +0900

    feat: add flake for develop

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "Flake for Aozora Typer";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      fenix,
+      ...
+    }:
+    let
+      # System types to support.
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (
+        system:
+        import nixpkgs {
+          inherit system;
+        }
+      );
+    in
+    {
+
+      # define formatter for all systems
+      formatter = forAllSystems (system: nixpkgsFor.${system}.nixfmt-rfc-style);
+
+      # define devShell for aysstem with packages
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.kicad
+              pkgs.pre-commit
+            ];
+
+            shellHook = ''
+              pre-commit install
+            '';
+          };
+        }
+      );
+    };
+}
